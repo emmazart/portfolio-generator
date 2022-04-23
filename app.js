2516db817dc597250abce37a5dbe40c3c02956da
Emma Olson <emma.olson794@gmail.com>
2022-04-23 14:43:48 -0500

    initial js file

---
+++ app.js
@@ -0,0 +1,15 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+console.log(profileDataArgs);
+
+
+const printProfileData = profileDataArr => {
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+    // for (let i = 0; i < profileDataArr.length; i++) {
+    //     console.log(profileDataArr[i]);
+    // }
+};
+
+printProfileData(profileDataArgs);
