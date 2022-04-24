885cac64ddd3e3abee36c8bdae91faa4f7cfc514
Emma Olson <emma.olson794@gmail.com>
2022-04-24 16:10:19 -0500

    write generate html and modularize code

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [firstName, github] = profileDataArgs;
 
-console.log(profileDataArgs);
 
-
-const printProfileData = profileDataArr => {
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-    // for (let i = 0; i < profileDataArr.length; i++) {
-    //     console.log(profileDataArr[i]);
-    // }
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(firstName, github), err => {
+    if (err) throw err;
+    
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
